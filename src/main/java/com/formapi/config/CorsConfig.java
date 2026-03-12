b4ce71b4c55592918130b9bb5f99d9dcfb4c80b9
Juan Pablo Gil Félix <141673798+A01741619@users.noreply.github.com>
2026-03-12 17:39:12 -0600

    listo

--- src/main/java/com/formapi/config/CorsConfig.java
+++ src/main/java/com/formapi/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Aplica a todos los endpoints
-                        .allowedOrigins("http://localhost:4200") // URL de tu frontend
+                        .allowedOrigins("") // URL de tu frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // Permitir cookies/auth si es necesario
@@ -21,3 +21,5 @@ public class CorsConfig {
         };
     }
 }
+
+
--- src/main/java/com/formapi/controller/FormController.java
+++ src/main/java/com/formapi/controller/FormController.java
@@ -1,11 +1,11 @@
 package com.formapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.formapi.services.FormService;
 import java.util.Map;
 
@@ -16,9 +16,8 @@ public class FormController {
     @Autowired
     private FormService formService;
 
-    @PostMapping("/submit"){
-        public Map<String, String> submitForm(@RequestBody Map<String, Object> formData) {
-            return formService.processForm(formData);
-        }
-    }        
+    @PostMapping("/submit")
+    public Map<String, String> submitForm(@RequestBody Map<String, Object> formData) {
+        return formService.processForm(formData);
+    }
 }
